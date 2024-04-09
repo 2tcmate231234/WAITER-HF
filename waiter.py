823b0fddb030e82589bd5eb4bcf05c78e69ae084
2tcmate231234 <127303737+2tcmate231234@users.noreply.github.com>
2024-04-09 20:30:56 +0200

    Update waiter.py

--- waiter.py
+++ waiter.py
@@ -38,6 +38,14 @@ def osszes_bevetel(osszegek):
 
 
 def kiir(elso, utolso,ossz,ö_f,ossz_bev):
+    print(osszegek)
+    print(fizetett)
+    print(pennyk)
+    print(f"{szmlt_penny}")
+    print(otosok)
+    print(maximalis)
+    print(_8_23_val)
+    print(kilenc_i)
     print(f"Az óra végen {osszegzes} lett a pénztárcájá-ban")
     if elso != -1:
         print(f"Az első tíz fontnál többet fizető vendég a, {elso + 1}  volt.")
@@ -49,6 +57,46 @@ def kiir(elso, utolso,ossz,ö_f,ossz_bev):
     print(f"A nap végén a pincér {bevetel} fontot kapott")
 
 
+
+def pincerfizet(l):
+    fizetett=False
+    for elem in l:
+        if elem<0:
+            fizetett=True
+    return fizetett
+        
+def kapott_penny(l):
+    kapott=False
+    for elem in l:
+        if elem!=int(elem):
+            kapott=True
+    return kapott
+    
+def penny_szmll(l):
+    szum=0
+    for elem in l:
+        szum+=elem-int(elem)
+    return int(100*szum)
+    
+def legalabb_5(l):
+    db=0
+    for elem in l:
+        if elem>=5:
+            db+=1
+    return db
+
+def legnagyobb_szamla(l):
+    return max(l)
+
+def _8_23_val_sum(l):
+    return 8.23+sum(l)
+
+def i_fizet_9(l):
+    i=0
+    while i<len(l) and l[i]!=9:
+        i+=1
+    return i+1
+
 # Fő program
 
 # 1 input
@@ -59,6 +107,15 @@ osszegzes = szamitas(osszegek)
 elso, utolso = eldontes(osszegek)
 ot_font=ot_fontos_fizetes(osszegek)
 bevetel=osszes_bevetel(osszegek)
+osszegek=beolvasas()
+fizetett=pincerfizet(osszegek)
+pennyk=kapott_penny(osszegek)
+szmlt_penny=penny_szmll(osszegek)
+otosok=legalabb_5(osszegek)
+maximalis=legnagyobb_szamla(osszegek)
+_8_23_val=_8_23_val_sum(osszegek)
+kilenc_i=i_fizet_9(osszegek)
+
 
 # 3 output
 kiir(elso, utolso,osszegzes,ot_font,bevetel)
